00d6a9d4fcd6122d6c6e8d93f8631632a7f145b1
Mark-Alexis-Posadas <markalexisposadas@yahoo.com>
2024-03-19 15:22:16 +0800

    added data and reducer function

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,95 @@
+import { useReducer } from "react";
+import Input from "./components/Input";
+
+type InitialStateType = {
+  firstName: string;
+  lastName: string;
+  email: string;
+  userName: string;
+  password: string;
+  confirmPassword: string;
+  gender: string;
+  submittedData: any; // You might want to replace 'any' with a more specific type
+  theme: boolean;
+};
+
+const data = [
+  {
+    id: 1,
+    text: "First Name",
+  },
+  {
+    id: 2,
+    text: "Last Name",
+  },
+  {
+    id: 3,
+    text: "User Name",
+  },
+  {
+    id: 4,
+    text: "Email",
+  },
+  {
+    id: 5,
+    text: "Password",
+  },
+  {
+    id: 6,
+    text: "Confirm Password",
+  },
+];
+
+const initialState: InitialStateType = {
+  firstName: "",
+  lastName: "",
+  email: "",
+  userName: "",
+  password: "",
+  confirmPassword: "",
+  gender: "",
+  submittedData: null,
+  theme: false,
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "UPDATE_FIELD":
+      return {
+        ...state,
+        [action.field]: action.value,
+      };
+    case "SUBMIT_FORM":
+      return {
+        ...state,
+        submittedData: { ...state },
+      };
+    case "TOGGLE_THEME":
+      return { ...state, theme: !state.theme };
+    default:
+      return state;
+  }
+}
+
 export default function App() {
-  return <div>hello</div>;
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <form>
+      {data.map((d) => (
+        <div key={d.id}>
+          <label>{d.text}</label>
+          <Input
+            placeholder="first name"
+            name="firstName"
+            id="first_name"
+            type="text"
+            className="w-full p-2 bg-white shadow-md"
+            value={state[d.field]}
+          />
+        </div>
+      ))}
+      <button className="text-white bg-purple-600 rounded p-2">Submit</button>
+    </form>
+  );
 }
--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,3 +1,28 @@
-export default function Input() {
-  return <input>Input</input>;
+interface InputProps {
+  type: string;
+  name: string;
+  id: string;
+  placeholder: string;
+  value: string | number;
+  className: string;
+}
+
+export default function Input({
+  type,
+  name,
+  id,
+  placeholder,
+  value,
+  className,
+}: InputProps) {
+  return (
+    <input
+      type={type}
+      name={name}
+      id={id}
+      placeholder={placeholder}
+      value={value}
+      className={className}
+    />
+  );
 }
